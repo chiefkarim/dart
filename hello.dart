b56f81396e9c499bc02782ceea03b140a1d46236
chiefkarim <krimotop2016@gmail.com>
2024-12-27 21:02:02 +0100

    feat: day 2

--- hello.dart
+++ hello.dart
@@ -1,10 +1,24 @@
-String hello(String name){
-  return 'Welcome MR.${name}'; 
+import 'goodbay.dart' as goodbay;
 
-  }
+String hello([String? nono]) {
+  String? name = nono;
+  name ??= 'karim';
+  return 'Welcome MR.${name}';
+}
+
+class Todo {
+  final String who;
+  final String what;
+
+  const Todo(this.who, this.what);
+}
 
 void main() {
-  String printStatment=hello('karim');
-  print(printStatment);
-
+  var todo1 = new Todo("karim", "dish washing");
+  print(
+    todo1,
+  );
+  goodbay.main();
 }
+
+/// so this is the [hello] method
